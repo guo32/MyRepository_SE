a2cfd9d4b58003b4def08c69de0cad7b1931dbea
guo32 <guo32@users.noreply.github.com>
2022-06-16 16:53:39 +0900

    class C 변경, A에서 C 생성자 테스트

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -2,5 +2,7 @@
 public class A {
 	public static void main(String args[]) {
 		System.out.println("Class A");
+		C c1 = new C();
+		c1.printC();
 	}
 }
--- MyGit/src/C.java
+++ MyGit/src/C.java
@@ -2,5 +2,6 @@
 public class C {
 	public void printC() {
 		System.out.println("Print class C.");
+		System.out.println("Test C.");
 	}
 }
